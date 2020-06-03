3bfbb63084f2c3c23952ef9b0188b0b62d4ec112
jawastew <20376569+Jawastew@users.noreply.github.com>
2020-06-03 21:00:19 +0200

    Release version: Bugfixes:  - updated to Text HUD API v2 (as v1 support was dropped), this makes the mod functional again ;) Changes:  - added default fontsize from LCD Panel font size  - support for text shadows  - Debug and Monofont font support, this is retrieved from the LCD Panel font  - Circumvent SE shaders for clearer text

--- Data/Scripts/HudLcd/HudLcd.cs
+++ Data/Scripts/HudLcd/HudLcd.cs
@@ -8,8 +8,11 @@ using VRage.ModAPI;
 using VRageMath;
 
 using Draygo.API;
+using BlendTypeEnum = VRageRender.MyBillboard.BlendTypeEnum;
 
 using Jawastew;
+using System.Text;
+
 namespace Jawastew.HudLcd
 {
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TextPanel), false)]
@@ -18,7 +21,8 @@ namespace Jawastew.HudLcd
         //Workshop & Debug
         const long WORKSHOP_ID = 911144486;
         const string MOD = "HudLcd";
-        static bool logging = Logger.logging(true);
+        static bool debug = false;
+        bool debugTrigger = debug;
 
         //Static
         public static bool IsInit => textAPI != null;
@@ -27,25 +31,32 @@ namespace Jawastew.HudLcd
 
         static HudAPIv2 textAPI = null;
 
-        //config
-        const int ttl = 10;
-        const string matchstring = "hudlcd";
-        const char delim = ':';
-        const double textPosX = -0.98;
-        const double textPosY = -0.2;
-        const double textScale = 0.8;
+        // Defaults &  Config Format
+        const string configTag = "hudlcd";
+        const char configDelim = ':';
+        const double textPosXDefault = -0.98;
+        const double textPosYDefault = -0.2;
+        const double textScaleDefault = 0.8;
+        const string textFontDefault = "white";
+        const bool textFontShadowDefault = false;
 
-        // Textpanel
-        IMyTextPanel thisLcd = null;
+        // Textpanel Current
+        IMyTextPanel thisTextPanel = null;
         HudAPIv2.HUDMessage thisHUDMessage;
-        Vector2D thistextPosition = new Vector2D(-0.98, -0.2);
-        double thistextScale = 0.8;
-        string thisconfigcolour = Color.White.ToString();
+        Vector2D thisTextPosition = new Vector2D(textPosXDefault, textPosYDefault);
+        double thisTextScale = textScaleDefault;
+        string thisTextcolour = null;
+        string thisTextFont = textFontDefault;
+        bool thisTextFontShadow = textFontShadowDefault;
 
         IMyTerminalBlock ControlledEntity => MyAPIGateway.Session.LocalHumanPlayer.Controller.ControlledEntity as IMyTerminalBlock;
-        bool isControlled => ControlledEntity != null && ControlledEntity.CubeGrid == thisLcd.CubeGrid;
-        bool hasHudLcd = false;
+        bool IsControlled => ControlledEntity != null && ControlledEntity.CubeGrid == thisTextPanel.CubeGrid;
 
+        // Cache
+        string configCache = null;
+        double textScaleCache = textScaleDefault;
+        string textFontCache = textFontDefault;
+        StringBuilder thisMessage = null;
 
 
         //Initialises textAPI once for clients, checks if is Server
@@ -64,7 +75,8 @@ namespace Jawastew.HudLcd
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
             base.Init(objectBuilder);
-            thisLcd = Entity as IMyTextPanel;
+            thisTextPanel = Entity as IMyTextPanel;
+            configCache = null; // reset cache
             NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
         }
 
@@ -79,53 +91,101 @@ namespace Jawastew.HudLcd
         {
             if (isServer) return;
             if (!IsAPIAlive) return;
-            UpdateValues();
-            if (isControlled && hasHudLcd)
-                UpdateLCD();
+            if (debugTrigger)
+            {
+                if (debug) MyAPIGateway.Utilities.ShowMessage("HudLcd", "New TextPanel" + thisTextPanel.GetPublicTitle());
+                debugTrigger = false;
+            }
+            if (IsControlled && UpdateValues())
+                UpdateHUDMessage();
+            else
+                PurgeHUDMessage();
         }
 
-        private void UpdateValues()
-        {
-            hasHudLcd = false;
-            if (thisLcd.GetPublicTitle().ToLower().Contains(matchstring))
-            {
-                hasHudLcd = true;
-                ParseAndUpdateConfig(thisLcd.GetPublicTitle());
-            }
-            else if (thisLcd.CustomData.ToLower().Contains(matchstring))
-            {
-                hasHudLcd = true;
-                ParseAndUpdateConfig(thisLcd.CustomData);
-            }
-        }
 
-        private void ParseAndUpdateConfig(string data)
+        /*  UpdateValues()
+         *  returns true when a valid configuration for hudlcd has been found (and set).
+         *  returns false otherwise.
+         */
+        private bool UpdateValues()
         {
-            String[] lines = data.Split('\n');
+            // try and fetch new configuration
+            string config;
+            if (thisTextPanel.GetPublicTitle() != null && thisTextPanel.GetPublicTitle().ToLower().Contains(configTag))
+            {
+                config = thisTextPanel.GetPublicTitle();
+            }
+            else if (thisTextPanel.CustomData != null && thisTextPanel.CustomData.ToLower().Contains(configTag))
+            {
+                config = thisTextPanel.CustomData;
+            }
+            else
+            {
+                // no hudlcd config found.
+                return false;
+            }
+            
+            thisTextScale = thisTextPanel.FontSize;
+
+            switch (thisTextPanel.Font)
+            {
+                case "Monospace":
+                    thisTextFont = "monospace";
+                    break;
+                default:
+                    thisTextFont = textFontDefault;
+                    break;
+            }
+
+            // purge HUDMessage, if change detected
+            if (config != configCache || textScaleCache != thisTextScale || textFontCache != thisTextFont)
+            {
+                if (debug) MyAPIGateway.Utilities.ShowMessage("HudLcd", "Detected Config Change: " + config + thisTextPanel.Font);
+                PurgeHUDMessage();
+            }
+
+            // Update Cache
+            configCache = config;
+            textScaleCache = thisTextScale;
+            textFontCache = thisTextFont;
+
+            // Get config from config string
+            String[] lines = config.Split('\n');
             foreach (String line in lines)
             {
-                if (line.ToLower().Contains(matchstring))
+                if (line.ToLower().Contains(configTag))
                 {
-                    String[] rawconf = line.Split(delim);
-                    for (int i = 0; i < 5; i++)
+                    if (debug && !config.Equals(configCache)) MyAPIGateway.Utilities.ShowMessage("HudLcd", "Detected Config Change: " + line.Substring(line.IndexOf(configTag)));
+                    String[] rawconf = line.Substring(line.IndexOf(configTag)).Split(configDelim); // remove everything before hudlcd in the string.
+                    for (int i = 0; i < 6; i++)
                     {
-                        if (rawconf.Length > i) // Set values from Config Line
+                        if (rawconf.Length > i && rawconf[i].Trim() != "") // Set values from Config Line
                         {
                             switch (i)
                             {
                                 case 0:
                                     break;
                                 case 1:
-                                    thistextPosition.X = trygetdouble(rawconf[i], textPosX);
+                                    thisTextPosition.X = trygetdouble(rawconf[i], textPosXDefault);
                                     break;
                                 case 2:
-                                    thistextPosition.Y = trygetdouble(rawconf[i], textPosY);
+                                    thisTextPosition.Y = trygetdouble(rawconf[i], textPosYDefault);
                                     break;
                                 case 3:
-                                    thistextScale = trygetdouble(rawconf[i], textScale);
+                                    thisTextScale = trygetdouble(rawconf[i], textScaleDefault);
                                     break;
                                 case 4:
-                                    thisconfigcolour = "<color=" + rawconf[i].Trim() + ">";
+                                    thisTextcolour = "<color=" + rawconf[i].Trim() + ">";
+                                    break;
+                                case 5:
+                                    if (rawconf[i].Trim() == "1")
+                                    {
+                                        thisTextFontShadow = true;
+                                    }
+                                    else
+                                    {
+                                        thisTextFontShadow = false;
+                                    }
                                     break;
                                 default:
                                     break;
@@ -138,17 +198,20 @@ namespace Jawastew.HudLcd
                                 case 0:
                                     break;
                                 case 1:
-                                    thistextPosition.X = textPosX;
+                                    thisTextPosition.X = textPosXDefault;
                                     break;
                                 case 2:
-                                    thistextPosition.Y = textPosY;
+                                    thisTextPosition.Y = textPosYDefault;
                                     break;
                                 case 3:
-                                    thistextScale = textScale;
+                                    thisTextScale = thisTextPanel.FontSize;
                                     break;
                                 case 4:
-                                    var fontColor = thisLcd.GetValueColor("FontColor");
-                                    thisconfigcolour = $"<color={fontColor.R},{fontColor.G},{fontColor.B}>";
+                                    var fontColor = thisTextPanel.GetValueColor("FontColor");
+                                    thisTextcolour = $"<color={fontColor.R},{fontColor.G},{fontColor.B}>";
+                                    break;
+                                case 5:
+                                    thisTextFontShadow = false;
                                     break;
                                 default:
                                     break;
@@ -156,10 +219,11 @@ namespace Jawastew.HudLcd
                         }
 
                     }
-                    break;
+                    break; // stop processing lines from Custom Data
                 }
 
             }
+            return true;
 
         }
 
@@ -175,17 +239,52 @@ namespace Jawastew.HudLcd
             }
         }
 
-        private void UpdateLCD()
+        private void UpdateHUDMessage()
         {
-            System.Text.StringBuilder sb = new System.Text.StringBuilder(thisconfigcolour, 500);
-            thisLcd.ReadPublicText(sb, true);
-            thisHUDMessage = new HudAPIv2.HUDMessage(sb, thistextPosition, null, ttl, thistextScale,false,false,Color.Black);
+            if (thisMessage == null)
+            {
+                thisMessage = new StringBuilder(500);
+            }
+            thisMessage.Clear();
+            thisMessage.Append(thisTextcolour);
+            thisTextPanel.ReadText(thisMessage, true);
+            if (thisHUDMessage == null)
+            {
+                if (debug) MyAPIGateway.Utilities.ShowMessage("HudLcd", "Drawing hud with: " + thisMessage);
+                thisHUDMessage = new HudAPIv2.HUDMessage(
+                    Message: thisMessage,
+                    Origin: thisTextPosition, 
+                    Offset: null,
+                    TimeToLive: -1,
+                    Scale: thisTextScale,
+                    HideHud: false,
+                    Shadowing: thisTextFontShadow,
+                    ShadowColor: Color.Black,
+                    Blend: BlendTypeEnum.PostPP,
+                    Font: thisTextFont
+                    );
+            }
+        }
+
+        private void PurgeHUDMessage()
+        {
+            if (thisHUDMessage != null)
+            {
+                thisHUDMessage.Visible = false;
+                thisHUDMessage.DeleteMessage();
+                thisHUDMessage = null;
+            }
         }
 
         public override void Close()
         {
-            textAPI.Unload();
+            PurgeHUDMessage();
         }
 
     }
+
+    // [MyEntityComponentDescriptor(typeof(MyObjectBuilder_LCDPanelsBlock), false)]
+
 }
+
+
